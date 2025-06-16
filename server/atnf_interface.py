337ec51e1a28c6a17ce6ab4b89911001fc810884
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2025-06-16 21:42:19 +0200

    Added flask server as backend to fetch data from ATNF database

---
+++ server/atnf_interface.py
@@ -0,0 +1,56 @@
+"""Flask backend to fetch data from the ATNF database"""
+
+import numpy as np
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import psrqpy
+
+PARAMETERS = [
+    "PSRJ",
+    "RAJ",  # Position
+    "DECJ",
+    "GL",  # Galactic position
+    "GB",
+    "P0",  # Period
+    "DM",  # Dispersion Measure
+    "DIST",  # Distance proxies
+    "PX",
+    "S1400",  # Brightness at 1400 MHz
+    "AGE",  # Derived characteristics
+    "BSURF",
+]
+
+app = Flask(__name__)
+CORS(app, origins="*")
+
+
+def pulsar_data_to_dict(data) -> dict:
+    """Parse data into a clean dictionary"""
+    return {
+        "name": data["PSRJ"][0],
+        "position": {"ra": data["RAJ"][0], "dec": data["DECJ"][0]},
+        "galactic_position": {"gl": data["GL"][0], "gb": data["GB"][0]},
+        "period_s": data["P0"][0],
+        "dispersion_measure": data["DM"][0],
+        "distance_kpc": data["DIST"][0] or data["PX"][0],
+        "flux_density_1400": data["S1400"][0],
+        "characteristics": {"age": data["AGE"][0], "surface_mag_field": data["BSURF"][0]},
+    }
+
+
+@app.route("/pulsar")
+def get_pulsar():
+    """Get pulsar from the ATNF library"""
+    name = request.args.get("name")
+
+    try:
+        query = psrqpy.QueryATNF(params=PARAMETERS, psrs=[name])
+        dataframe = query.pandas
+        data = dataframe.replace({np.nan: None})
+
+        return jsonify(pulsar_data_to_dict(data))
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+
+app.run(port=5000)
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {"/pulsar": "http://127.0.0.1:5000"},
+  },
 })
